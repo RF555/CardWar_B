70bef1c8dff85ba619e9cc794a3cc893bd63f3ce
roey-ubuntu <roey.fenigold@gmail.com>
2023-04-03 16:41:23 +0300

    Upload TestCard.cpp

---
+++ TestCard.cpp
@@ -0,0 +1,22 @@
+#include "doctest.h"
+#include <stdexcept>
+#include "sources/player.hpp"
+#include "sources/game.hpp"
+#include "sources/card.hpp"
+
+using namespace std;
+using namespace ariel;
+
+TEST_CASE("Card initiated correctly") {
+    Card c1;
+    CHECK(c1.getRank() == -1);
+    CHECK(c1.getSuit() == SUIT_UNDEFINED);
+}
+
+TEST_CASE("Function 'setCard' works") {
+    Card c1;
+    c1.setCard(SPADE, 3);
+    CHECK(c1.getRank() == THREE);
+    CHECK(c1.getSuit() == SPADE);
+
+}
