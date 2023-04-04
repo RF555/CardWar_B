c3a2e7e60575f92d1a4cb3ec63fbe14e5ee17dba
roey-ubuntu <roey.fenigold@gmail.com>
2023-04-04 14:38:56 +0300

    Update Game

--- sources/game.hpp
+++ sources/game.hpp
@@ -9,42 +9,41 @@ namespace ariel {
     class Game {
     public:
         Player p1, p2;
-//    Card deck[52];
 
         Game(Player _p1, Player _p2);
 
 //        ~Game();
 
-/**
-* @brief Simulate a turn in the game.
-*/
+        /**
+        * @brief Simulate a turn in the game.
+        */
         void playTurn();
 
-/**
-* @brief Print the last turn stats.
-*/
+        /**
+        * @brief Print the last turn stats.
+        */
         void printLastTurn();
 
-/**
-* @brief Plays the game until the end.
-*/
+        /**
+        * @brief Plays the game until the end.
+        */
         void playAll();
 
-/**
-* @brief Print the name of the winning player.
-*/
+        /**
+        * @brief Print the name of the winning player.
+        */
         void printWiner();
 
-/**
-* @brief Print all the turns played one line per turn
-     * (same format as game.printLastTurn()).
-*/
+        /**
+        * @brief Print all the turns played one line per turn
+        * (same format as game.printLastTurn()).
+        */
         void printLog();
 
-/**
-* @brief For each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand.
-     * (draw within a draw counts as 2 draws. ).
-*/
+        /**
+        * @brief For each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand.
+        * (draw within a draw counts as 2 draws. ).
+        */
         void printStats();
 
     };
